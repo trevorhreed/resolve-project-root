c7858d999fd5549cd97eb05838bf7d772f161215
Trevor <trevorhreed@gmail.com>
2017-10-25 16:45:37 -0600

    updating parameter name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = (PROJECT_DIRECTORY) => {
+module.exports = (root) => {
   const path = require('path')
   const originalPathResolve = path.resolve;
   path.resolve = (...parts) => {
     parts = parts.map((part)=>{
-      if(part.startsWith('~/')) part = path.join(PROJECT_DIRECTORY, part.substr(2));
+      if(part.startsWith('~/')) part = path.join(root, part.substr(2));
       return part;
     });
     return originalPathResolve(...parts);
